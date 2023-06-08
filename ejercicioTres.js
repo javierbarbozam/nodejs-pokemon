eb836a339b14f09b7340000dd054822e8a24ab70
javierbarbozam <barboza.javier497@gmail.com>
2023-06-08 02:16:58 -0600

    exercise 3 update

--- ejercicioTres.js
+++ ejercicioTres.js
@@ -1,17 +1,39 @@
-const axios = require('axios');
+const axios = require("axios");
 
-const url = 'https://pokeapi.co/api/v2/pokemon/';
+const url = "https://pokeapi.co/api/v2/pokemon/";
 
-function teams(){
+function getTeamMember(id) {
   axios
-  .get(url)
-  .then(response => {
-    const value = response.data.results
-    
-  })
-  .catch(error => {
-    console.error(error);
-  })
+    .get(`${url}${id}`)
+    .then((response) => {
+      const value = response.data;
+      console.log('pokemon team', renderTeam(value))
+    })
+    .catch((error) => {
+      console.error(error);
+    });
 }
 
-module.exports = teams;
+const pokemonCount = 1000;
+
+function randomId(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+function renderTeam (value) {
+  const team = [];
+  const pokemon = {
+    name: value.name,
+    id: value.id
+  }
+  team.push(pokemon)
+  return team
+}
+
+function pokemonTeam() {
+  for (let i = 0; i <= 5; i++) {
+    getTeamMember(randomId(1, pokemonCount));
+  }
+}
+
+module.exports = pokemonTeam;
--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const pokemon20Names = require('./ejercicioUno');
 const pokemonSearchName = require('./ejercicioDos');
+const pokemonTeam = require('./ejercicioTres')
 
 let readlineSync = require('readline-sync');
 //let question = readlineSync.question('Insert a pokemon name: ');
 
-pokemon20Names();
+// pokemon20Names();
 //pokemonSearchName(question)
+pokemonTeam()
